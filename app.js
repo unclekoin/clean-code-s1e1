6aa736de45cfddc1a6dc5ddb1a55eedbf09999b4
Pavel Koryakin <unclekoin@gmail.com>
2022-05-19 19:31:39 +0300

    refactor: adjust conciseness of identifier and class names according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -158,7 +158,7 @@ for (var i = 0; i < incompleteTaskHolder.children.length; i++) {
 
 //cycle over completedTasksHolder ul list items
 for (var i = 0; i < completedTasksHolder.children.length; i++) {
-  //bind events to list items children(tasksUncompleted)
+  //bind events to list items children(tasksIncomplete)
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
 
